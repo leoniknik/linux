e6b4fc58861759dbac3adb283d8464e7b599bbce
leoniknik <leoniknik@mail.ru>
2016-12-21 19:06:03 +0300

    19:02

--- init.sh
+++ init.sh
@@ -7,10 +7,14 @@ sudo apt-get install libapache2-mod-python
 sudo apt-get install nginx
 sudo apt-get install sysstat
 #apache2
+sudo rm /etc/apache2/apache2.conf
 sudo cp apache/apache2.conf /etc/apache2
+sudo rm /etc/apache2/ports.conf
 sudo cp apache/ports.conf /etc/apache2
+sudo rm /etc/apache2/sites-enabled/000-default.conf
 sudo cp apache/000-default.conf /etc/apache2/sites-enabled
 #nginx
+sudo rm /etc/nginx/sites-enabled/default
 sudo cp nginx/default /etc/nginx/sites-enabled
 #python
 sudo cp .htaccess /var/www/html
